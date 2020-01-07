eb61accfebe3f921f51065f2f14324c6f24b2591
Thomas Klijnsma <klijnsma@fnal.gov>
2020-01-07 13:50:56 -0600

    fixing job submission for subpackaging

---
+++ svj/core/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os.path as osp
+import os, logging
+
+from .logger import setup_logger, setup_subprocess_logger, set_log_file
+logger = setup_logger()
+subprocess_logger = setup_subprocess_logger()
+
+
+from . import utils
+
+def tarball(outfile=None, dry=False):
+    """ Wrapper function to create a tarball of svj.core """
+    return utils.tarball(__file__, outfile=outfile, dry=dry)
