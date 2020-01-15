82697e58671ca03e7af94fb0e9e4d67322adf296
Thomas Klijnsma <klijnsma@fnal.gov>
2020-01-15 14:46:36 -0600

    updated env script with more modern pip

--- env.sh
+++ env.sh
@@ -5,7 +5,11 @@ setup() {
     source /cvmfs/cms.cern.ch/cmsset_default.sh
     source "${gccsetup}"
     source "${rootsetup}"
-    export PATH="${pipdir}/bin:${pythondir}/bin:${PATH}"
+    if [ -z "$pythondir" ]; then
+        export PATH="${pipdir}/bin:${PATH}"
+    else
+        export PATH="${pipdir}/bin:${pythondir}/bin:${PATH}"
+    fi
     export PYTHONPATH="${pipdir}/lib/python2.7/site-packages/:${PYTHONPATH}"
     echo "Setup for architecture ${SCRAM_ARCH}"
     pip(){ ${pipdir}/bin/pip "$@"; }  # To avoid any local pip installations
@@ -15,13 +19,20 @@ if [[ "$SCRAM_ARCH" == "slc6"* ]]; then
     pythondir=/cvmfs/sft.cern.ch/lcg/releases/Python/2.7.15-c333c/x86_64-slc6-gcc62-opt
     pipdir=/cvmfs/sft.cern.ch/lcg/releases/pip/9.0.1-e2f3e/x86_64-slc6-gcc62-opt
     gccsetup=/cvmfs/sft.cern.ch/lcg/contrib/gcc/6.2/x86_64-slc6/setup.sh
-    rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc62-opt/bin/thisroot.sh
+    # rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc62-opt/bin/thisroot.sh
+    rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_88/ROOT/6.08.06/x86_64-slc6-gcc62-opt/ROOT-env.sh
     setup
+# Old setup:
+# elif [[ "$SCRAM_ARCH" == "slc7"* ]]; then
+#     pythondir=/cvmfs/sft.cern.ch/lcg/releases/Python/2.7.15-c333c/x86_64-centos7-gcc7-opt
+#     gccsetup=/cvmfs/sft.cern.ch/lcg/contrib/gcc/7/x86_64-centos7/setup.sh
+#     pipdir=/cvmfs/sft.cern.ch/lcg/releases/pip/9.0.1-e2f3e/x86_64-centos7-gcc7-opt
+#     rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_95/ROOT/6.16.00/x86_64-centos7-gcc7-opt/bin/thisroot.sh
+# New setup using ROOT-env.sh (which also sets python) and newer pip
 elif [[ "$SCRAM_ARCH" == "slc7"* ]]; then
-    pythondir=/cvmfs/sft.cern.ch/lcg/releases/Python/2.7.15-c333c/x86_64-centos7-gcc7-opt
-    pipdir=/cvmfs/sft.cern.ch/lcg/releases/pip/9.0.1-e2f3e/x86_64-centos7-gcc7-opt
     gccsetup=/cvmfs/sft.cern.ch/lcg/contrib/gcc/7/x86_64-centos7/setup.sh
-    rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_95/ROOT/6.16.00/x86_64-centos7-gcc7-opt/bin/thisroot.sh
+    pipdir=/cvmfs/sft.cern.ch/lcg/releases/pip/19.0.3-06476/x86_64-centos7-gcc7-opt
+    rootsetup=/cvmfs/sft.cern.ch/lcg/releases/LCG_95/ROOT/6.16.00/x86_64-centos7-gcc7-opt/ROOT-env.sh
     setup
 else
     echo "No known setup for arch ${SCRAM_ARCH}. Either use a supported architecture (slc6*, slc7*) or review the environment setup in $0"
